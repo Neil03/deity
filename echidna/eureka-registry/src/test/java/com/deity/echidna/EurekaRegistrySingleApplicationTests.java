c15f9def503d02670e6bd79cdfc44ff19e6cda5d
neil_yue <neil_yue@163.com>
2019-12-13 14:04:09 +0800

    Initial commit

---
+++ echidna/eureka-registry/src/test/java/com/deity/echidna/EurekaRegistrySingleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deity.echidna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaRegistrySingleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
